c755f2ac5207d008488f9acf04c177a6a80816b9
mmizzle9 <mmizzle9@gmail.com>
2014-05-18 00:46:20 -0500

    Initial Commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+    public float Speed;
+
+    void Awake()
+    {
+        rigidbody2D.velocity = new Vector2(Speed, Speed);
+    }
+
+    void Update()
+    {
+        var velocity = rigidbody2D.velocity;
+        if (velocity.x <= 0)
+            velocity.x = -Speed;
+        else velocity.x = Speed;
+
+        if (velocity.y <= 0)
+            velocity.y = -Speed;
+        else velocity.y = Speed;
+
+        rigidbody2D.velocity = velocity;
+    }
+}
---
+++ Assets/Scripts/PickedUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;  
+using System.Collections;
+
+public class PickedUp : MonoBehaviour
+{
+    private PlayerController _player;
+
+    void Start()
+    {
+        _player = FindObjectOfType<PlayerController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+            _player.NumPickups++;
+        }
+    }
+}
